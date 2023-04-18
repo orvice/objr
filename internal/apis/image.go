6cf491bc0ce53388758b34446307272326275c57
orvice <orvice@orx.me>
2023-04-18 13:15:32 +0800

    upload image apis (#1)
    * upload image apis
    
    * Create Dockerfile
    
    * Create go.yml
    
    * add cdn base url
    
    * add cdn base url

---
+++ internal/apis/image.go
@@ -0,0 +1,46 @@
+package apis
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/orvice/objr/internal/object"
+	"golang.org/x/exp/slog"
+)
+
+func uploadImage(c *gin.Context) {
+	// single file
+	f, _ := c.FormFile("image")
+	slog.Info("upload file", "file", f.Filename, "size", f.Size)
+
+	// gen object name with date
+	now := time.Now()
+	objectName := fmt.Sprintf("images/%d/%d/%d/%s", now.Year(), now.Month(), now.Day(), f.Filename)
+	dst := fmt.Sprintf("/tmp/%s", f.Filename)
+	// Upload the file to specific dst.
+	err := c.SaveUploadedFile(f, dst)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	ret, err := object.Upload(c.Request.Context(), objectName, dst, f.Size)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	// clean dst
+	err = os.Remove(dst)
+	if err != nil {
+		slog.Error("remove file error", "err", err)
+	}
+	c.JSON(200, gin.H{
+		"message": "success",
+		"url":     ret.URL,
+	})
+}
---
+++ internal/object/minio.go
@@ -0,0 +1,59 @@
+package object
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"golang.org/x/exp/slog"
+)
+
+var (
+	minioClient *minio.Client
+	bucket      string
+	cdnBaseURL  string
+)
+
+func Init() error {
+	endpoint := os.Getenv("S3_ENDPOINT")
+	accessKeyID := os.Getenv("S3_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("S3_ACCESS_KEY")
+	bucket = os.Getenv("S3_BUCKET")
+	cdnBaseURL = strings.TrimRight(os.Getenv("CDN_BASE_URL"), "/")
+	useSSL := true
+
+	var err error
+	// Initialize minio client object.
+	minioClient, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil // minioClient is now set up
+}
+
+type UploadResult struct {
+	URL string
+}
+
+func Upload(ctx context.Context, objectName string, filePath string, objectSize int64) (*UploadResult, error) {
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0)
+	if err != nil {
+		return nil, err
+	}
+	uploadInfo, err := minioClient.PutObject(ctx, bucket, objectName, file, objectSize, minio.PutObjectOptions{})
+	if err != nil {
+		return nil, err
+	}
+	slog.Info("upload success", "uploadInfo.key", uploadInfo.Key)
+
+	return &UploadResult{
+		URL: fmt.Sprintf("%s/%s", cdnBaseURL, uploadInfo.Key),
+	}, nil
+}
